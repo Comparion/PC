ba64ba33db807e02591ab4232d3f466189c27011
Patryk <patryk_banas@o2.pl>
2021-01-11 15:32:55 +0100

    Initial commit

---
+++ src/Computer.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private Monitor monitor;
+    private Drive drive;
+    private HeadPhones headPhones;
+
+    List<USBDevice> usbDevices = new ArrayList<>();
+
+    public Computer(Monitor monitor, Drive drive) {
+        this.monitor = monitor;
+        this.drive = drive;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public Drive getDrive() {
+        return drive;
+    }
+
+    public void setDrive(Drive drive) {
+        this.drive = drive;
+    }
+
+    public HeadPhones getHeadPhones() {
+        return headPhones;
+    }
+
+    public void setHeadPhones(HeadPhones headPhones) {
+        this.headPhones = headPhones;
+    }
+
+    public List<USBDevice> getUsbDevices() {
+        return usbDevices;
+    }
+}
---
+++ src/HeadPhones.java
@@ -0,0 +1,12 @@
+
+public class HeadPhones {
+    private final String name;
+
+    public HeadPhones(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
